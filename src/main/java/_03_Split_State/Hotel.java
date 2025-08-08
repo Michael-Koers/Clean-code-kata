2442e4c55351bb8f6c11a0a783757154d336dfba
Michael Koers <michael.koers@befrank.nl>
2025-08-08 10:54:58 +0200

    Remove usage of Lombok

--- src/main/java/_03_Split_State/Hotel.java
+++ src/main/java/_03_Split_State/Hotel.java
@@ -1,16 +1,49 @@
 package _03_Split_State;
 
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.Getter;
-
-@Data
-@AllArgsConstructor
 public class Hotel {
 
     private String name;
     private int rooms;
     private int floors;
     private int employees;
+
+    public Hotel(final String name, final int rooms, final int floors, final int employees) {
+        this.name = name;
+        this.rooms = rooms;
+        this.floors = floors;
+        this.employees = employees;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(final String name) {
+        this.name = name;
+    }
+
+    public int getRooms() {
+        return rooms;
+    }
+
+    public void setRooms(final int rooms) {
+        this.rooms = rooms;
+    }
+
+    public int getFloors() {
+        return floors;
+    }
+
+    public void setFloors(final int floors) {
+        this.floors = floors;
+    }
+
+    public int getEmployees() {
+        return employees;
+    }
+
+    public void setEmployees(final int employees) {
+        this.employees = employees;
+    }
 }
--- src/main/java/_04_Nulls/Item.java
+++ src/main/java/_04_Nulls/Item.java
@@ -1,16 +1,20 @@
 package _04_Nulls;
 
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 
-@Getter
-@RequiredArgsConstructor
 public class Item {
 
     private final int id;
     private final String name;
     private final Double price;
     private final Double discount;
+
+    public Item(final int id, final String name, final Double price, final Double discount) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.discount = discount;
+    }
 
     public Double getPriceWithDiscount() {
         if (discount == null) {
@@ -19,11 +23,23 @@ public class Item {
         return price - discount;
     }
 
-    public String getName(){
-        if(name == null){
+    public String getName() {
+        if (name == null) {
             return null;
         }
 
         return name.toLowerCase();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public Double getDiscount() {
+        return discount;
     }
 }
